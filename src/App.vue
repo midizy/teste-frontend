e6a813630d4edb268fafb452b90399006bf297e6
Isabelle Berta <bertaisabelle@gmail.com>
2019-11-11 20:56:09 -0300

    update

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>RELATÓRIO</h1>
+    <ExAlunos />
+  </div>
+
+</template>
+<script>
+import ExAlunos from './components/ExAlunos.vue'
+export default {
+  name: 'app',
+  components: {
+    ExAlunos
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  font-family: 'Optima', sans-serif;
+  width: 1000px;
+  text-align:center;
+  color:rgb(13, 3, 56);
+}
+
+</style>
---
+++ src/components/ExAlunos.vue
@@ -0,0 +1,70 @@
+<template>
+<table>
+    <thead>
+    <tr>
+      <th>Regional</th>
+      <th>Média</th>
+    </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="exalunosData in exalunosDataList.regionals" :key="exalunosData.description" class="exalunos-data">
+      <td>{{exalunosData.description}}</td>
+      <td>{{exalunosData.average}}</td>
+      </tr>
+    </tbody>
+</table>
+</template>
+<script>
+export default {
+  name: "ExAlunos",
+  data() {
+    return {
+      exalunosDataList: []
+    };
+  },
+  created: function() {
+    this.getExAlunosData();
+  },
+  methods: {
+    getExAlunosData() {
+      fetch("exalunos.json")
+        .then(response => response.json())
+        .then(data => {this.exalunosDataList = data;
+         })
+   
+  }
+  }
+};
+</script>
+<style>
+table {
+  font-family: 'Optima', sans-serif;
+  width: 750px;
+  border-collapse: collapse;
+  border: 3px solid #44475C;
+  margin: auto;
+
+}
+
+table th {
+  text-transform: uppercase;
+  text-align: center;
+  background: #44475C;
+  color: #FFF;
+  padding: 8px;
+  min-width: 30px;
+}
+
+table td {
+  text-align: center;
+  padding: 8px;
+  border-right: 2px solid #7D82A8;
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #D4D8F9;
+}
+</style>
